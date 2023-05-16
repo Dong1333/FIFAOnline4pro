1e53dad94a452de4a096c614ff47587bc4be3c5e
seodong-geun <mogijisung13@gmail.com>
2023-05-16 10:29:05 +0900

    [05.16(화)] - 티어 정보 DTO 생성

--- src/main/java/com/example/fifaonline4pro/dto/DivisionDTO.java
+++ src/main/java/com/example/fifaonline4pro/dto/DivisionDTO.java
@@ -2,21 +2,16 @@ package com.example.fifaonline4pro.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
 
 /**
  * @version 1.0
  * @Author seodong-geun
- * @since 2023/05/11
+ * @since 2023/05/15
+ * 티어 정보를 담는 데이터 전송 객체(DTO)
  */
 @Getter
 @Setter
 public class DivisionDTO {
-        private String name;
-        private int divisionId;
-
-        public DivisionDTO(String name, int divisionId) {
-            this.name = name;
-            this.divisionId = divisionId;
-        }
+    private int divisionId; // 티어 식별 아이디
+    private String divisionName; // 티어 이름
 }
