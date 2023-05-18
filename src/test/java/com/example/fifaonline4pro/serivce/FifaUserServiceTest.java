24fc3ac116b9b6443cc5b30f58aa34a618ce5afe
seodong-geun <mogijisung13@gmail.com>
2023-05-18 14:31:06 +0900

    [05.18(목)] - 테스트 코드 클래스 생성, 작성(패키지, 파일)

---
+++ src/test/java/com/example/fifaonline4pro/serivce/FifaUserServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.fifaonline4pro.serivce;
+
+import com.example.fifaonline4pro.service.FifaUserService;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @version 1.0
+ * @Author seodong-geun
+ * @since 2023/05/18
+ */
+
+@SpringBootTest
+@Slf4j
+@ToString
+public class FifaUserServiceTest {
+
+        @Autowired
+        private FifaUserService fifaUserService;
+
+        @Test
+        public void testGetUserMatchHistory() {
+            // given
+            String accessId = "3640c1f174814c953bad41f9";
+            int matchType = 50;
+            int offset = 0;
+            int limit = 10;
+
+            // when
+            List<String> matches = fifaUserService.getUserMatchHistory(accessId, matchType, offset, limit);
+
+            // then
+            for (String matchId : matches) {
+                System.out.println(matchId);
+            }
+        }
+}
