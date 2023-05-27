c46bd85f28316025277fae1e423ce896cf20049f
seodong-geun <mogijisung13@gmail.com>
2023-05-27 20:00:56 +0900

    [05.24(토)] - 테스트 별 DisplayName 추가

--- src/test/java/com/example/fifaonline4pro/serivce/FifaUserServiceTest.java
+++ src/test/java/com/example/fifaonline4pro/serivce/FifaUserServiceTest.java
@@ -1,17 +1,22 @@
 package com.example.fifaonline4pro.serivce;
 
+import com.example.fifaonline4pro.dto.match.MatchDTO;
 import com.example.fifaonline4pro.service.FifaUserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.ToString;
-import lombok.extern.log4j.Log4j2;
+
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 /**
  * @version 1.0
@@ -26,21 +31,43 @@ public class FifaUserServiceTest {
 
         @Autowired
         private FifaUserService fifaUserService;
+        @Autowired
+        private HttpServletRequest request;
 
         @Test
+        @DisplayName("유저의 매칭 기록을 조회")
         public void testGetUserMatchHistory() {
             // given
             String accessId = "3640c1f174814c953bad41f9";
             int matchType = 50;
-            int offset = 0;
-            int limit = 10;
+            int offset = 1;// 번째 부터
+            int limit = 50;// 까지 기록 추출
+            fifaUserService.setAccessIdToSession(request, accessId); // 세션에 accessId 저장
 
             // when
-            List<String> matches = fifaUserService.getUserMatchHistory(matchType, offset, limit);
+            List<String> matches = fifaUserService.findUserMatchHistory(matchType, offset, limit);
 
             // then
+            log.info("--------(matches)------");
             for (String matchId : matches) {
+
                 System.out.println(matchId);
             }
         }
+
+        @Test
+        @DisplayName("유저의 매칭 아이디로 경기상세기록 조회")
+        public void testGetMatchInfo() throws JsonProcessingException {
+            // given
+            String matchId = "646c9362f49851157eb8a9a1";
+
+            // when
+            MatchDTO matchDTO = fifaUserService.findMatchInfo(matchId);
+
+            // then [ObjectMapper 클래스를 사용하여 'matchDTO' 객체를 JSON 문자열로 변환]
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonOutput = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(matchDTO);
+            log.info("matchDTO:\n" + jsonOutput);
+        }
+
 }
